647ae765f065eadd4d21c527849904f5462fda28
brotzky <brotzky@gmail.com>
2019-07-31 16:11:54 -0700

    Adding basepath back to index

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       options: {
         contentPosts: "content/posts",
         contentAuthors: "content/authors",
-        basePath: "/test",
+        basePath: "/",
       },
     },
     {
